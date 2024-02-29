78ee7c37c5c4b6eb8386942e92c0ca7553e2c787
curtis clayton <curtisclayton4270@gmail.com>
2024-02-28 23:18:20 -0500

    Added background , fixed spelling errors

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import Header from "./Header";
 import Footer from './Footer';
+import styles from "../style.module.scss";
+
 
 const Layout = ({children}) => {
     return (
         <>
             <Header />
             {children}
-            <div className="wave"></div>
-            <div className="wave"></div>
-            <div className="wave"></div>
+            <div className={styles.wave}></div>
+            <div className={styles.wave}></div>
+            <div className={styles.wave}></div>
+            
             <Footer /> 
         </>
     );
